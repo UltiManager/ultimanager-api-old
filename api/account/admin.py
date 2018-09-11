31f2e494739fe1c9bb8a64ec7d7b01a70ef211fb
Chathan Driehuys <chathan@driehuys.com>
2018-09-11 16:55:43 -0400

    Fix admin registrations.
    Admin models are now registered properly and functioning.

--- api/account/admin.py
+++ api/account/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import ugettext_lazy as _
 
 from account import models
 
@@ -8,6 +11,7 @@ class EmailAdmin(admin.ModelAdmin):
     """
     Admin for the Email model.
     """
+    autocomplete_fields = ('user',)
     fields = (
         'address',
         'user',
@@ -16,4 +20,51 @@ class EmailAdmin(admin.ModelAdmin):
         'time_updated',
     )
     list_display = ('address', 'user', 'is_verified')
+    list_filter = ('is_verified',)
+    readonly_fields = ('time_created', 'time_updated')
     search_fields = ('address', 'user__name')
+
+
+class UserAddForm(UserCreationForm):
+    class Meta:
+        fields = ('name',)
+        model = models.User
+
+
+@admin.register(models.User)
+class UserAdmin(auth_admin.UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'fields': ('name', 'password1', 'password2'),
+        }),
+    )
+    add_form = UserAddForm
+    autocomplete_fields = ('primary_email',)
+    date_hierarchy = 'time_created'
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'password'),
+        }),
+        (_('Personal Information'), {
+            'fields': ('primary_email',),
+        }),
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        }),
+    )
+    list_display = (
+        'name',
+        'primary_email',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'time_created',
+    )
+    ordering = None
+    search_fields = ('name',)
